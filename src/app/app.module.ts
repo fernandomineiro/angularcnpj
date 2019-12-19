6776e3c112feb3bca36d86f8737b98f7ffdb219c
fernando <fernandofitilan@hotmail.com>
2019-12-19 16:13:25 -0300

    upando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxMaskModule } from 'ngx-mask';
 import { options } from './options';
+import {NgxMaskModule} from 'ngx-mask';
 
 
 @NgModule({
